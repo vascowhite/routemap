af5511a8277b9c299f2052bd1c278b9a66317ac3
Paul White <paul@vascowhite.co.uk>
2018-02-05 15:15:35 +0000

    Added test for loading json from URL

--- tests/test_routemap.py
+++ tests/test_routemap.py
@@ -1,6 +1,7 @@
 """
 Tests for routemap package
 """
+import json
 import unittest
 from unittest.mock import MagicMock
 from unittest.mock import patch
@@ -46,6 +47,26 @@ class TestRoutemap(unittest.TestCase):
 
         test_pos = "23 30.0N 34 15.0W"
         self.assertEqual((23.5, -34.25), routemap.get_current_position(test_pos))
+
+    @patch('requests.get')
+    def test_can_get_positions_from_url(self, mock_requests):
+        with open('tests/test.json') as jsonf:
+            test_positions = json.load(jsonf)
+
+        test_lons = [-79.591, -79.6, -79.613, -79.718, -79.918]
+        test_lats = [8.996, 9.005, 9.017, 9.117, 9.209]
+        test_annots = [(-79.591, 8.996, 'Start'), (-79.918, 9.209, 'End')]
+        mock_requests.return_value = MagicMock(
+            spec=Response,
+            status_code=200,
+            json=MagicMock(return_value=test_positions)
+        )
+
+        lons, lats, annots = routemap.parseurl('http://anyurlwilldo.com/positions')
+
+        self.assertEqual(test_lons, lons)
+        self.assertEqual(test_lats, lats)
+        self.assertEqual(test_annots, annots)
 
 
 if __name__ == '__main__':
