75724e72795a4fdc63122ec1ca6037a81ececb99
Paul White <paul@vascowhite.co.uk>
2018-03-08 16:39:10 +0000

    Add some default text for  currposlabel

--- routemap/routemap.py
+++ routemap/routemap.py
@@ -255,7 +255,7 @@ def get_current_position(posstr):
 def plot(
             filename,
             currpos=None,
-            currposlabel='',
+            currposlabel='Current Position',
             output=None,
             display=None,
             custtitle=None,
