1e52eaf9759621ce6babb0e628935e100a361a66
Paul White <paul@vascowhite.co.uk>
2018-05-29 17:56:43 +0100

    Bump to 0.0.3

--- routemap/__version__.py
+++ routemap/__version__.py
@@ -5,7 +5,7 @@ Version information
 __title__ = 'routemap'
 __description__ = 'Generate routemaps from various sources.'
 __url__ = 'https://github.com/vascowhite/routemap'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Paul White'
 __author_email__ = 'routemap@vascowhite.co.uk'
 __license__ = 'MIT'
